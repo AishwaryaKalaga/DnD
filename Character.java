eb1ef960b11495bf18ca15af44aa54c81d750608
AishwaryaKalaga <s-kalagaa@bsd405.org>
2023-05-19 12:44:13 -0700

    Update Character.java

--- Character.java
+++ Character.java
@@ -2,31 +2,28 @@ import java.io.Serializable;
 public class Character implements Serializable {
     private String race;
     private String name;
-    private int level;
+    private static int level;
     private int health;
-    private int strength;
-    private int defense;
+    //private int strength;
+    //private int defense;
 
-    public Character(){
-        race = null;
-        name = null;
-        level = 1;
-        health = 0;
-        strength = 0;
-        defense = 0;
-    }
-
-    public String Character (String race, Stringname){
+    public Character (String race, String name){
         this.race = race;
         this.name = name;
-        getLevel();
-
-
+        level=0;
 
     }
+    public void assign () {
+        switch (race) {
+            case "";
+        }
+    }
     public String raiseLevel() {
-        level=1;       
+        level++;       
         return "Congrats! Your level is now "+level;
 
+    }
+    public String getCharacter() {
+        String "Your character " +name +" is a " +race;
     }
 }
