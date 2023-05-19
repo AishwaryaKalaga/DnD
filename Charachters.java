57e9ccf0c8350f68ab067e691cc7063aa1964741
AishwaryaKalaga <s-kalagaa@bsd405.org>
2023-05-19 11:18:00 -0700

    Update Charachters.java

---
+++ Charachters.java
@@ -0,0 +1,14 @@
+public class Charachter {
+    private String name;
+    private String class;
+    private int health;
+    private int level;
+
+    public Charachter(String name, String class, int health, int level) {
+        this.name=name;
+        this.class=class;
+        this.health=health;
+        this.level=level;
+    }
+    
+}
