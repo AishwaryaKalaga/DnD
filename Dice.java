7aee4af0fe21a8eb1fec1b770eaa92fe07f961d7
hayl2 <s-cowanh@bsd405.org>
2023-05-19 12:43:09 -0700

    roll 4-10

--- Dice.java
+++ Dice.java
@@ -1,3 +1,31 @@
 public class Dice {
+    private int num;
     
+    public int roll4(){
+        num = Math.random() * 4 + 1;
+        return num;
+    }
+    
+    public int roll6(){
+        num = Math.random() * 6 + 1;
+        return num;
+    }
+
+    public int roll8(){
+        num = Math.random() * 8 + 1;
+        return num;
+    }
+
+    public int roll10(){
+        num = Math.random() * 8 + 1;
+        return num;
+    }
+
+
+
+
+
+
+
+
 }
