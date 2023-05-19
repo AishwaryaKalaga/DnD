f95b8556e8d39a06ecab2dce91f0c5e471a06fc7
hayl2 <s-cowanh@bsd405.org>
2023-05-19 08:48:40 -0700

    adding character class

--- Main.java
+++ Main.java
@@ -10,29 +10,29 @@ public class Main{
         String response = scan.nextLine();
         boolean ans = true;
         while(ans){
-            if (response.equals("y")){
+            if (response.equalsIgnoreCase("y")){
                 ans = false;
                 //Character selection, switch statments
                 System.out.println("Pick your character:");
                 System.out.println("bard");
                 System.out.println("fighter");
-                System.out.println("mage");
+                System.out.println("ranger");
                 System.out.println("wizard");
                 System.out.println("rogue");
                 String character = scan.nextLine();
 
                 switch(character){
                     case "bard":
-                        System.out.println("You selected bard!");
+                        System.out.println("You have been assigned bard!");
                         break;
                     case "fighter":
-                        System.out.println("You selected fighter!");
+                        System.out.println("You have been assigned fighter!");
                         break;
-                    case "mage":
-                        System.out.println("You selected mage!");
+                    case "ranger":
+                        System.out.println("You have been assigned ranger!");
                         break;
                     case "wizard":
-                        System.out.println("You selected wizard!");
+                        System.out.println("You haved been assigned wizard!");
                         break;
                     default:
                         System.out.println("You have been assigned rogue");
@@ -40,7 +40,7 @@ public class Main{
                 }
 
             }
-            else if (response.equals("n")){
+            else if (response.equalsIgnoreCase("n")){
                 ans = false;
                 System.out.println("End of program");
                 break;
