1bb55512619e4b954affad66add75ddceb69f71e
AishwaryaKalaga <107134903+AishwaryaKalaga@users.noreply.github.com>
2023-05-30 14:31:36 -0700

    Update Character.java

--- Character.java
+++ Character.java
@@ -4,20 +4,39 @@ public class Character implements Serializable {
     private String name;
     private static int level;
     private int health;
-    //private int strength;
-    //private int defense;
+
 
     public Character (String race, String name){
         this.race = race;
         this.name = name;
         level=0;
+        health=0;
 
     }
-    public void assign () {
+    public void assignHealth () {
         switch (race) {
-            case "":
+            case "fighter":
+                break;
+                health=100;
+            case "bard":
+                health=90;
+                break;
+            case "ranger":
+                break;
+                health=75;
+            case "wizard":
+                break;
+                health=50;
+            default :
+                health=120;
+                break;
+            health=health*(level/5)
         }
     }
+    public int getHealth () {
+        return health;
+    }
+
     public String raiseLevel() {
         level++;       
         return "Congrats! Your level is now "+level;
